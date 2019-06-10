af57bb8e7b44d101fa1df7b1195a08616c47f7b6
Matt Williams <matt.seongjun@gmail.com>
2019-06-10 12:03:32 -0400

    Flesh out controller

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,2 +1,20 @@
 class CouponsController < ApplicationController
+    def index
+        @coupons = Coupon.all
+    end
+
+    def show
+        @coupon = Coupon.find_by_id(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+        @coupon = Coupon.create(
+            coupon_code: params[:coupon_code],
+            store: params[:store]
+        )
+        redirect_to coupons_path(@coupon)
+    end
 end
